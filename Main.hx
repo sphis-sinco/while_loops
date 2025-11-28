b78931ed4ff76215d3b878cda770e9bb2cb613b8
Viwu <sinconsistencia@gmail.com>
2025-11-28 09:32:01 -0600

    standard IO streams

--- Main.hx
+++ Main.hx
@@ -1,5 +1,14 @@
 package;
 
 class Main {
-	public static function main() {}
+	public static function main() {
+		// Printing without using trace, useful to print without a newline character
+		Sys.stdout().writeString('Please type your name: ');
+		// Flush characters to terminal
+		Sys.stdout().flush();
+
+		// Reading user input through terminal
+		final input = Sys.stdin().readLine();
+		trace('Hello ${input}}!');
+	}
 }
